c83be0cbd3cc1dccc78e1ef7ea33d629ef46bf9f
Wilhelm Hedman <hedmanw@student.chalmers.se>
2016-02-29 11:37:10 +0100

    Logger propagates messages

--- src/main/java/se/alkohest/irkk/util/Logger.java
+++ src/main/java/se/alkohest/irkk/util/Logger.java
@@ -1,7 +1,22 @@
 package se.alkohest.irkk.util;
 
 public class Logger {
+    private static EventListener listener;
+
+    public static void setEventListener(EventListener listener) {
+        Logger.listener = listener;
+    }
+
     public static void log(String tag, String msg) {
-        System.out.println(tag + ":" + msg); // TODO: register and send upwards
+        if (listener == null) {
+            System.out.println(tag + ":" + msg);
+        }
+        else {
+            listener.log(tag, msg);
+        }
+    }
+
+    public interface EventListener {
+        void log(String tag, String message);
     }
 }
